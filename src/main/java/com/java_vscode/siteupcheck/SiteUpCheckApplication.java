f04da1802e06d4676c50a900d3c4fde9c0a7b362
Gabor <szemes.gabor@yahoo.com>
2021-06-02 17:58:43 +0200

    Init Java-VSCode check-project

---
+++ src/main/java/com/java_vscode/siteupcheck/SiteUpCheckApplication.java
@@ -0,0 +1,13 @@
+package com.java_vscode.siteupcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteUpCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteUpCheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java_vscode/siteupcheck/SiteUpCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java_vscode.siteupcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteUpCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
